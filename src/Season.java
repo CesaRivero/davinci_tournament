060f634e5560b09d03f608a97c0fd53f04ba04b5
Cesar Rivero <cesar_tomas_rivero@hotmail.com>
2022-10-17 10:32:28 -0300

    add: clases

---
+++ src/Season.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Season {
+    private ArrayList<Match> matchs;
+    public void addMatch(Match match) {
+        //agregar a la lista
+        return;
+    }
+
+}
