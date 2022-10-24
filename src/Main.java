f6559cd225871f0ba0c239745a7db778542cbcb6
Cesar Rivero <cesar_tomas_rivero@hotmail.com>
2022-10-24 10:26:23 -0300

    feat: add teams

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,33 @@ public class Main {
         TeamSeason estudiantesFC = new TeamSeason("Estudiantes FC",0);
         TeamSeason recursantesFC = new TeamSeason("Recursantes FC");
 
+        //creamos jugadores equipo 1
         Player profeSergio=new Player("Sergio Medina",1,"Goalkeeper");
-        Player profeSuplente=new Player("Benito Camela",13,"Goalkeeper");
+        Player profeSuplente=new Player("Benito Camela",13,"Central Defender");
+        Player profeWeb= new Player("Pedro Perez",5,"Central Defender");
+        Player profeUx= new Player("Julio Casares",8,"Midfielder");
+        Player profeJava = new Player("Elda Montoto",9,"Forward");
 
+        //agregamos jugadores al equipo 1
         profesoresPOO.addPlayer(profeSergio);
         profesoresPOO.addPlayer(profeSuplente);
+        profesoresPOO.addPlayer(profeWeb);
+        profesoresPOO.addPlayer(profeUx);
+        profesoresPOO.addPlayer(profeJava);
+
+        //creamos jugadores equipo 2
+        Player maxi=new Player("Maxi Grippo",1,"Goalkeeper");
+        Player franco=new Player("Franco",5,"Central Defender");
+        Player nachoide = new Player( "Ignacio Armendariz",4,"Central Defender");
+        Player cesarRivero=new Player("Cesar Rivero",10,"Midfielder");
+        Player luisDiaz=new Player("Luis Diaz",9,"Forward");
+
+        //agregamos jugadores a equipo 2
+        estudiantesFC.addPlayer(maxi);
+        estudiantesFC.addPlayer(franco);
+        estudiantesFC.addPlayer(nachoide);
+        estudiantesFC.addPlayer(cesarRivero);
+        estudiantesFC.addPlayer(luisDiaz);
 
       /*  profesoresPOO.setPosition(1);
         estudiantesFC.setPosition(0); agregue a constructor*/
