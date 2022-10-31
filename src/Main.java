5c75044f01eae90b528da474269785bf49350ae5
Cesar Rivero <cesar_tomas_rivero@hotmail.com>
2022-10-31 10:56:42 -0300

    feat: player goal counter

--- src/Main.java
+++ src/Main.java
@@ -48,9 +48,9 @@ public class Main {
        /* primerParcial.setLocalTeam(profesoresPOO);
         primerParcial.setVisitantTeam(estudiantesFC); agregamos como constructor */
 
-        primerParcial.makeLocalGoal(profesoresPOO.getPlayers().get(0));
-        primerParcial.makeLocalGoal(profesoresPOO.getPlayers().get(0));
-        primerParcial.makeVisitantGoal(estudiantesFC.getPlayers().get(0));
+        primerParcial.makeLocalGoal(0);
+        primerParcial.makeLocalGoal(4);
+        primerParcial.makeVisitantGoal(1);
 
         System.out.println(primerParcial.generateMarker());
 
